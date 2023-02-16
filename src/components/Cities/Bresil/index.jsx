1406a36474269821e22044252b172ef5256102b5
Proactivee <arnaud.deprost@gmail.com>
2023-02-16 21:45:36 +0100

    Town

--- src/components/Cities/Bresil/index.jsx
+++ src/components/Cities/Bresil/index.jsx
@@ -25,15 +25,32 @@ const Town = styled(Link)`
   padding: 20px;
   border-radius: 15px;
   cursor: pointer;
+  margin:10px;
+`
+const Town2 = styled(Link)`
+  text-decoration:none;
+  font-family:'November',sans-serif;
+  font-size:30px;
+  margin:10px;
+  @media (max-width: 768px) {
+    font-size:20px;
+  }
+  color: black;
+  background: #deddd7;
+  width: fit-content;
+  padding: 20px;
+  border-radius: 15px;
+  cursor: default;
 `
 function Cities() {
  
-  //const Ilha = () => {
-  // window.location = '/wtam/bresil/Ilha'
-  // }
+  
   return (
     <Container>
       <Town to={'/wtam/bresil/saopolo'}>São Paulo</Town>
+      <Town2 >Paraty</Town2>
+      <Town2 >Ilha Grande</Town2>
+      <Town2 >Rio de Janeiro</Town2>
     </Container>
   )
 }
